3af011a77f7be2bbd0952f3d0e7b3e2aab096489
Snaos91 <armsnaos@yandex.ru>
2022-03-02 18:09:37 +0300

    D7
    D7

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'news',
-    'account'
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -45,6 +45,9 @@ class Post(models.Model):
     def preview(self):
         return self.text[0:123] + '...'
 
+    def get_absolute_url(self):
+        return f'/news/{self.id}'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
